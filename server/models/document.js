25936a39b9812ccac60fb1bc803b5dad13fbb013
Putti Manisha <puttimanisha90@gmail.com>
2026-06-05 14:18:41 +0530

    Day 3: Create Document model

---
+++ server/models/document.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const documentSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      required: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Document", documentSchema);
