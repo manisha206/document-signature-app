12807a90ba8e86d26520abd866441cc00fd1d89b
Putti Manisha <puttimanisha90@gmail.com>
2026-06-05 13:08:28 +0530

    Day 2: Implement registration API

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -10,13 +11,19 @@ connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
+// Authentication Routes
+app.use("/api/auth", authRoutes);
+
+// Server Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
